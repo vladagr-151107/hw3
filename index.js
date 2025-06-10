65eaa7f8d68635e2f68407fc984039031ce991da
Vlada Grushchenko <vladagrushchenko@gmail.com>
2025-06-10 23:00:17 +0300

    task2

---
+++ index.js
@@ -0,0 +1,4 @@
+const input1 = prompt("Enter the first string");
+const input2 = prompt("Enter the second string");
+const input3 = prompt("Enter the third string");
+console.log(`${input2}, ${input1}, ${input3}`);
