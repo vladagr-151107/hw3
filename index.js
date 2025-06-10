a97a635910043325a1710c5975fd422beb9f5ed1
Vlada Grushchenko <vladagrushchenko@gmail.com>
2025-06-10 23:51:53 +0300

    task3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict";
 const input1 = prompt("Enter the first string");
 const input2 = prompt("Enter the second string");
 const input3 = prompt("Enter the third string");
---
+++ task3.js
@@ -0,0 +1,4 @@
+"use strict";
+const number = 12345;
+const digits = String(number).split('').join(' ');
+console.log(digits);
